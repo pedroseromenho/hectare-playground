25d6a0949501229760481c85ad5c4e05fbd86ac9
pedroseromenho <seromenho.pedro@gmail.com>
2021-02-21 20:21:46 +0100

    add config anim desktop menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: [`_site/**/*.html`],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      red: "#d86047",
+      green: "#d3df9f",
+      yellow:  "#f9f4d6",
+      orange: "#e18e75",
+      purple: "#5c56a1",
+    },
     extend: {},
   },
   variants: {
