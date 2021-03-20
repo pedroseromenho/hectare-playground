df5091abb250c5a79c0febb2ce4456e348fc1704
pedroseromenho <seromenho.pedro@gmail.com>
2021-03-20 23:31:00 +0100

    deploy gh-pages

--- docs/docs/docs/docs/js/nav.js
+++
@@ -1,8 +0,0 @@
-const updateMenuTextColor = () => {
-    const _page = window.location.pathname;
-    const [{ text: color }] = config.filter(({page}) => _page.includes(`/${page}/`)) || []; 
-    const nav = document.querySelector('.ap-nav');
-      nav.classList.add(`text-${color}`);
-}
-
-updateMenuTextColor();
